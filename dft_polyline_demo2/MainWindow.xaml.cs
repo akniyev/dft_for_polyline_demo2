3f7417f41d26963727e4597e629711e60af92e66
Hasan <hasan.akniyev@gmail.com>
2016-05-09 22:34:37 +0300

    I have added ncalc package for extression evaluation and make some changes in GUI

--- dft_polyline_demo2/MainWindow.xaml.cs
+++ dft_polyline_demo2/MainWindow.xaml.cs
@@ -20,14 +20,82 @@ namespace dft_polyline_demo2
     /// </summary>
     public partial class MainWindow : Window
     {
-        void EnableDisableControls()
+        void enableDisableControls()
         {
-            
+            if (radPolyNodesRandom.IsChecked == true)
+            {
+                txtMaxDelta.IsEnabled = true;
+                comboNodesFunction.IsEnabled = false;
+                btnNodesFunctionSave.IsEnabled = false;
+            }
+            else
+            {
+                txtMaxDelta.IsEnabled = false;
+                comboNodesFunction.IsEnabled = true;
+                btnNodesFunctionSave.IsEnabled = true;
+            }
+
+            if (radPolyValuesRandom.IsChecked == true)
+            {
+                txtPolyMinValue.IsEnabled = true;
+                txtPolyMaxValue.IsEnabled = true;
+                comboValuesFunction.IsEnabled = false;
+                btnValuesFunctionSave.IsEnabled = false;
+            }
+            else
+            {
+                txtPolyMinValue.IsEnabled = false;
+                txtPolyMaxValue.IsEnabled = false;
+                comboValuesFunction.IsEnabled = true;
+                btnValuesFunctionSave.IsEnabled = true;
+            }
+        }
+
+        bool noErrors()
+        {
+            var errorMessages = new Tuple<string,int>[]
+            {
+                new Tuple<string, int>("m - integer, m >= 1", 0),
+                new Tuple<string, int>("h = 0 || h >= 2pi/m", 0),
+                new Tuple<string, int>("min_x, min_y - double, min_x < min_y", 0),
+                new Tuple<string, int>("Nodes should be in ascending order", 0),
+                new Tuple<string, int>("Wrong nodes function", 0),
+                new Tuple<string, int>("Wrong values function", 0),
+                new Tuple<string, int>("2n <= N in FT", 0),
+                new Tuple<string, int>("2n <= N in DFT", 0),
+            };
+
+            return true;
         }
 
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void radPolyNodesRandom_Click(object sender, RoutedEventArgs e)
+        {
+            enableDisableControls();
+        }
+
+        private void radPolyNodesFunction_Click(object sender, RoutedEventArgs e)
+        {
+            enableDisableControls();
+        }
+
+        private void radPolyValuesRandom_Click(object sender, RoutedEventArgs e)
+        {
+            enableDisableControls();
+        }
+
+        private void radPolyValuesFunction_Click(object sender, RoutedEventArgs e)
+        {
+            enableDisableControls();
+        }
+
+        private void Window_Activated(object sender, EventArgs e)
+        {
+            enableDisableControls();
         }
     }
 }
